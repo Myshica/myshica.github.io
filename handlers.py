281edf34647301ffee6a761bed86bcd38cea7608
zangetsu <dimongames6@gmail.com>
2024-02-12 20:36:10 +0300

    New

--- handlers.py
+++ handlers.py
@@ -10,7 +10,7 @@ start_router = Router()
 @start_router.message(CommandStart())
 async def start(message: Message):
     keyboard = InlineKeyboardBuilder()
-    keyboard.row(InlineKeyboardButton(text="Открыть преложение", web_app=WebAppInfo(url="https://beautiful-lobster.static.domains/site")))
+    keyboard.row(InlineKeyboardButton(text="Открыть преложение", web_app=WebAppInfo(url="https://myshica.github.io/site.html")))
     await message.answer("Здравствуйте!", reply_markup=keyboard.as_markup())
 
 
